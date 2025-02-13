16aa1768fd5ec88638ded471f9d1e70be892b969
Daniel Rodriguez <danielrxd0912@gmail.com>
2025-02-13 11:01:21 -0400

    Design Header and Hero section

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+import { LiaBarsSolid, LiaBell, LiaShoppingCartSolid, LiaUser } from 'react-icons/lia';
+
 export default function Header() {
   return (
-    <header></header>
+    <header className="flex bg-white fixed w-full items-center justify-between py-5 px-12 shadow-md">
+      <Link to="/" className="text-2xl font-bold text-font">
+        Delux<span className="text-primary">Shop</span>
+      </Link>
+      <div className="flex items-center gap-6 max-md:hidden">
+        <i>
+          <LiaBell className="text-2xl cursor-pointer" />
+        </i>
+        <i>
+          <LiaShoppingCartSolid className="text-2xl cursor-pointer" />
+        </i>
+        <span>
+          <LiaUser className="text-2xl cursor-pointer" />
+        </span>
+      </div>
+      <div className="hidden max-md:block">
+        <i>
+          <LiaBarsSolid className="text-2xl" />
+        </i>
+      </div>
+    </header>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,30 @@
+import { LiaArrowRightSolid } from "react-icons/lia";
+
 export default function Home() {
   return (
-    <section>
+    <>
+      <section className="bg-white text-font h-screen grid md:grid-cols-2 w-full justify-center px-24 items-center ">
+        <section className="">
+          <h1 className="text-6xl font-extrabold">Tecnología que te inspira</h1>
+          <p className="text-xl font-light my-8">
+            {' '}
+            Descubre las últimas novedades y encuentra el producto perfecto para
+            ti.
+          </p>
+          <button className="bg-primary flex items-center gap-2 py-3 px-6 cursor-pointer rounded-md shadow-md text-white text-xl">
+            Shop now  
+            <LiaArrowRightSolid className="inline" />
+          </button>
+        </section>
+        <figure className="flex justify-center max-md:hidden">
+          <img
+            className="cursor-pointer rounded-4xl shadow-md w-[75%] h-[75%] hover:scale-105 ease-in-out duration-200"
+            src="/public/images/Hero-Image.png"
+            alt="Desktop Monitor"
+          />
+        </figure>
+      </section>
       Home Page
-    </section>
-  )
+    </>
+  );
 }
