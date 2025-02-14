43c97ecf18c30d4d7633c779cfd4934c9f3546b8
Daniel Rodriguez <danielrxd0912@gmail.com>
2025-02-14 10:48:25 -0400

    Design categories section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,26 +2,29 @@ import { LiaArrowRightSolid } from 'react-icons/lia';
 
 export default function Hero() {
   return (
-    <section className="bg-white text-font h-screen grid md:grid-cols-2 w-full justify-center px-24 items-center ">
-      <article className="items-center flex-col max-md:flex">
-        <h1 className="text-6xl font-extrabold">Tecnología que te inspira</h1>
-        <p className="text-xl font-light my-8 max-md:mb-16">
-          {' '}
-          Descubre las últimas novedades y encuentra el producto perfecto para
-          ti.
-        </p>
-        <button className="bg-primary flex items-center gap-2 py-3 px-6 cursor-pointer rounded-md shadow-md text-white text-xl hover:scale-105 ease-in-out duration-100">
-          Shop now
-          <LiaArrowRightSolid className="inline" />
-        </button>
-      </article>
-      <figure className="flex justify-center max-md:hidden">
-        <img
-          className="cursor-pointer rounded-4xl shadow-md w-[75%] h-[75%] hover:scale-105 ease-in-out duration-200"
-          src="/public/images/Hero-Image.png"
-          alt="Desktop Monitor"
-        />
-      </figure>
+    <section className='relative'>
+      {/* <img className='absolute -z-10 h-screen w-full object-cover' src="/public/images/cinema-desktop-2.webp" alt="wallpaper" /> */}
+      <section className="bg-transparent text-neutral-900 h-screen grid md:grid-cols-2 w-full justify-center px-24 items-center ">
+        <article className="items-center flex-col max-md:flex">
+          <h1 className="text-6xl font-extrabold">Tecnología que te inspira</h1>
+          <p className="text-xl font-light my-8 max-md:mb-16">
+            {' '}
+            Descubre las últimas novedades y encuentra el producto perfecto para
+            ti.
+          </p>
+          <button className="bg-primary flex items-center gap-2 py-3 px-6 cursor-pointer rounded-md shadow-md text-white text-xl hover:scale-105 ease-in-out duration-100">
+            Shop now
+            <LiaArrowRightSolid className="inline" />
+          </button>
+        </article>
+        <figure className="flex justify-center max-md:hidden">
+          <img
+            className="cursor-pointer rounded-4xl shadow-md w-[75%] h-[75%] hover:scale-105 ease-in-out duration-200"
+            src="/public/images/Hero-Image.png"
+            alt="Desktop Monitor"
+          />
+        </figure>
+      </section>
     </section>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,16 +11,39 @@ export default function Home() {
         <h2 className="text-6xl font-extrabold">Innovación a tu alcance</h2>
         <p className="py-4 text-neutral-600">Explora nuestra amplia selección de productos y encuentra la tecnología que te hará la vida más fácil.</p>
         <section className="grid grid-cols-2 max-md:grid-cols-1 w-[80%] min-h-64 gap-8 mt-6 justify-items-center">
-          <div className="w-full h-auto rounded-md bg-white shadow-md">Tvs</div>
-          <div className="w-full h-auto rounded-md bg-white shadow-md">Laptops</div>
-          <div className="w-full h-auto rounded-md bg-white shadow-md">Audio</div>
-          <div className="w-full h-auto rounded-md bg-white shadow-md">Gaming</div>
+          <div className="cursor-pointer w-full h-auto rounded-md bg-white shadow-md overflow-hidden relative">
+            <h4 className="absolute text-lg font-bold text-white lg:top-2 left-4 max-lg:bottom-2" >TV's</h4>
+            <p className="max-lg:hidden absolute text-4xl font-light text-white bottom-12 left-4">Vive el cine en casa</p>
+            <img className="object-cover h-full w-full" src="/public/images/visual-desktop.webp" alt="TV" />
+          </div>
+          <div className="cursor-pointer w-full h-auto rounded-md bg-white shadow-md overflow-hidden relative">
+            <h4 className="absolute text-lg font-bold text-white lg:top-2 left-4 max-lg:bottom-2" >Audio</h4>
+            <p className="max-lg:hidden absolute text-4xl font-light text-white bottom-4 left-4 drop-shadow-md">Sonido que <br /> te envuelve</p>
+            <img className="object-cover h-full w-full" src="/public/images/audio.webp" alt="TV" />
+          </div>
+          <div className="cursor-pointer w-full h-auto rounded-md bg-white shadow-md overflow-hidden relative">
+            <h4 className="absolute text-lg font-bold text-white lg:top-2 left-4 max-lg:bottom-2" >Smartphones</h4>
+            <p className="max-lg:hidden absolute text-4xl font-light text-white bottom-4 left-4 drop-shadow-md">Conéctate <br /> con el mundo</p>
+            <img className="object-cover h-full w-full" src="/public/images/phone-2.webp" alt="TV" />
+          </div>
+          <div className="cursor-pointer w-full h-auto rounded-md bg-white shadow-md overflow-hidden relative">
+            <h4 className="absolute text-lg font-bold text-white lg:top-2 left-4 max-lg:bottom-2" >Gaming</h4>
+            <p className="max-lg:hidden absolute text-2xl font-light text-white bottom-4 left-4 drop-shadow-md">El poder en tus manos</p>
+            <img className="object-cover h-full w-full" src="/public/images/gaming.webp" alt="TV" />
+          </div>
         </section>
       </section>
 
       <section className="bg-white px-4 md:px-[150px] py-16 flex flex-col gap-5 items-center text-neutral-900">
         <h3 className="text-4xl font-bold text-center">Calidad, variedad y los mejores precios en un solo lugar.</h3>
         <article className="grid mygrid w-full h-full gap-16 pt-10">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
